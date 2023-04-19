91a6db1681413ca24044601e6bd580a45f900cc2
adamacneill <adamichelee@gmail.com>
2023-04-18 19:05:46 -0700

    initial commit

---
+++ FactoryPatternExercise2/ListDataAccess.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public class ListDataAccess : IDataAccess
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+            new Product (){Name ="Xbox", Price = 350},
+             new Product (){Name ="Guitar", Price = 1000},
+              new Product (){Name ="Blender", Price = 50},
+               new Product (){Name ="Kite", Price = 25},
+
+        };
+        public List<Product> LoadData
+        {
+            get
+            {
+                Console.WriteLine("I am reading data from List Data Access");
+
+                return Products;
+            }
+        }
+
+        List<Product> IDataAccess.LoadData { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void SaveData()
+        {
+            Console.WriteLine("I am saving data from a List Data Access");
+        }
+    }
+}
--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -2,9 +2,43 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userInput;
+            bool correctInput;
+            do           
+            {
+                Console.Clear();
+                correctInput = true;
+                Console.WriteLine("What Database would you like to work with?");
+                Console.WriteLine("Type: sql");
+                Console.WriteLine("Type: mongo");
+                Console.WriteLine("Type: list");
+
+
+
+                userInput = Console.ReadLine();
+
+
+                if (userInput != "sql" && userInput != "mongo" && userInput != "list")
+                {
+                    correctInput = false;
+                    Console.WriteLine("Bad Input!");
+                    Thread.Sleep(1000);
+                }
+            } while (!correctInput);
+
+            Console.Clear();
+
+            IDataAccess db = DataAccessFactory.GetDataAccessType(userInput);
+
+            var products = db.LoadData;
+            db.SaveData();
+
+            foreach (var item in products)
+            {
+                Console.WriteLine($"Name: {item.Name} Price: {item.Price}");
+            }
         }
     }
 }
